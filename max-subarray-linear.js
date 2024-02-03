ef6477200d0dc9b544ef145794d7dfc65d67fc46
vipul prakash rai <vipulprakash.rai@gmail.com>
2024-02-03 23:34:51 +0530

    js challenge

--- max-subarray-linear.js
+++ max-subarray-linear.js
@@ -1,6 +1,5 @@
-//Write a function called maxSubarraySum that takes an array of integers and a 
-//positive integer k as input. The function should find the maximum sum of any
-// subarray of length k using an O(n^2) solution by using nested for loops.
+//Write a function called maxSubarraySum that takes an array of integers and a positive integer k as input. 
+//The function should find the maximum sum of any subarray of length k using an O(n) solution by using the sliding window technique.
 
 /*
 Examples
@@ -15,21 +14,27 @@ Constraints
 The input integer k will be between 1 and the length of the array.
 */
 
+
 //Solution->
 
 function maxSubarraySum(arr, k){
     let maxSum = 0;
-    for(let i = 0; i <= arr.length - k; i++){
-      let currentSum = 0;
-      for(let j = i; j < i + k; j++){
-        currentSum += arr[j];
-      }
+    let currentSum = 0;
+    for(let i = 0; i < k; i++){
+      maxSum += arr[i]; 
+    }
+    currentSum = maxSum;
+    for(let i = k; i < arr.length; i++){
+      currentSum = currentSum + arr[i] - arr[i - k]
       maxSum = Math.max(maxSum, currentSum);
     }
     return maxSum;
+    
   }
+  
+  
   const arr = [2, 5, 3, 1, 11, 7, 6, 4];
   const k = 3;
   console.log(maxSubarraySum(arr, k)); 
-  
-  //Time-Complexity-> O(n^2)
+
+  //Time-Complexity- O(n)
